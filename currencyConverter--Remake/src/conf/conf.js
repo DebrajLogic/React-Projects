b9154a69437894ad5953fe4571496c58dd56b436
Debraj <81313461+DebrajLogic@users.noreply.github.com>
2023-10-19 09:57:29 +0000

    added env variables

---
+++ currencyConverter--Remake/src/conf/conf.js
@@ -0,0 +1,4 @@
+const conf = {
+    apiUrl: String(import.meta.env.VITE_API_URL)
+}
+export default conf
---
+++ currencyConverter--Remake/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import conf from '../conf/conf'
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      `${conf.apiUrl} + ${currency}.json`
+    )
+      .then((response) => response.json())
+      .then((data) => setData(data[currency]))
+      .catch((error) => console.error("Error fetching currency info:", error));
+  }, [currency]);
+
+  return data !== null ? data : null;
+}
+
+export default useCurrencyInfo;
