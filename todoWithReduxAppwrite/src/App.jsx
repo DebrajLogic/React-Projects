26b9274f5b17bbdb16e4bccecaceb2a0b580dc12
Debraj <81313461+DebrajLogic@users.noreply.github.com>
2023-10-21 19:39:48 +0000

    Added redux

--- todoWithReduxAppwrite/src/App.jsx
+++ todoWithReduxAppwrite/src/App.jsx
@@ -1,13 +1,18 @@
-
+import { AddTodo, TodoList } from "./components";
 
 function App() {
- 
-
   return (
-    <>
-     
-    </>
-  )
+    <div className="h-screen w-full my-8 flex flex-col justify-start place-items-center">
+      <h1 className="bg-pink-600 w-full text-center text-white text-xl py-2 mb-10 rounded-lg">
+        Todo App with Appwrite as Backend
+      </h1>
+      <div>
+        <AddTodo />
+        <h1 className="text-center mt-4">TODOS:</h1>
+        <TodoList />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
